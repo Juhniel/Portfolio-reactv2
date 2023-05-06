f684857bff5a5aa3dca5d77665354538235f8226
Juhniel <juhn.sweden@gmail.com>
2023-05-07 01:17:11 +0200

    portfolio

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -6,7 +6,7 @@ export default function ThemeToggle({ handleThemeDark, handleThemeLight, theme }
     <button
       onClick={handleThemeLight}
     >
-      {theme === "light" ? <BsSun className="text-amber-400 w-8 text-[36px] m-2  dark:text-white hover:text-amber-600 dark:hover:text-amber-600"/> :  <BsSun className="text-amber-200 w-8 text-[36px] m-2  dark:text-white hover:text-yellow-400 dark:hover:text-yellow-400"/>}
+      {theme === "light" ? <BsSun className="text-amber-400 w-8 text-[36px] m-2 dark:text-white hover:text-amber-600 dark:hover:text-amber-600"/> :  <BsSun className="text-amber-200 w-8 text-[36px] m-2  dark:text-white hover:text-yellow-400 dark:hover:text-yellow-400"/>}
           
          
         
--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,84 +1,105 @@
-import React from 'react';
+import React from "react";
 
-import {motion} from "framer-motion"
-import {fadeIn} from "../variants"
+import { motion } from "framer-motion";
+import { fadeIn } from "../variants";
 import Img1 from "../assets/portfolio-img1.png";
 import Img2 from "../assets/portfolio-img2.png";
 import Img3 from "../assets/portfolio-img3.png";
 
+
+
 export default function Work() {
-  return(
-    <section id='work' className='section'>
+  return (
+    <section id="work" className="section">
       <div className="container mx-auto">
-        <div className='flex flex-col lg:flex-row gap-x-10'>
-          <div className='flex-1 flex flex-col gap-y-12 mb-10 lg:mb-0'>
+        <div className="flex flex-col lg:flex-row gap-x-12">
+          <motion.div 
+          variants={fadeIn("right", 0.3)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="flex-1 flex flex-col gap-y-10 mb-10 lg:mb-0">
             {/* text */}
             <div>
-              <h2 className='text leading-tight'>
-                My Latest <br/>
-                Work
+              <h2 className="h2 leading-tight text">
+              My Latest <br/>
+              Work. 
               </h2>
-              <p className='max-w-sm mb-16'>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora, ipsam.
+              <p className="max-w-sm mb-16 text">
+                Lorem ipsum dolor sit amet consectetur, 
+                adipisicing elit. Ut porro excepturi dolore 
+                quasi vel aperiam vero illum dolorem repellendus facilis.
               </p>
-              <button className='btn btn-sm'>View all projects</button>
+              <button className="btn btn-sm">View my work</button>
             </div>
             {/* image */}
-            <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl '>
+            <div className="group relative overflow-hidden dark:border-2 dark:border-white rounded-xl">
               {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full z-40 transition-all duration-300'></div>
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="" />
+              <img 
+              className="group-hover:scale-125 transition-all duration-500" 
+              src={Img1} 
+              alt="" 
+              />
               {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-white'>
-                UI/UX Design
-                </span>
-                </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-white">UI/UX Design</span>
+              </div>
               {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3xl text-white'>Project Title</span>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
-          </div>
-          <div className='flex-1 flex flex-col gap-y-10'>
+          </motion.div>
+          <motion.div 
+          variants={fadeIn("left", 0.5)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.3 }}
+          className="flex-1 flex flex-col gap-y-10 mb-10 lg:mt-[1.7rem]">
             {/* image */}
-          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+            <div className="group relative overflow-hidden dark:border-2 dark:border-white rounded-xl">
               {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full z-40 transition-all duration-300'></div>
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="" />
+              <img 
+              className="group-hover:scale-125 transition-all duration-500" 
+              src={Img2} 
+              alt="" 
+              />
               {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-white'>
-                UI/UX Design
-                </span>
-                </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-white">UI/UX Design</span>
+              </div>
               {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3xl text-white'>Project Title</span>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
-              {/* PROJECT 3 */}
-          <div className='group relative overflow-hidden border-2 border-white/50 rounded-xl'>
+             {/* image */}
+             <div className="group relative overflow-hidden dark:border-2 dark:border-white rounded-xl">
               {/* overlay */}
-              <div className='group-hover:bg-black/70 w-full h-full z-40 transition-all duration-300'></div>
+              <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
               {/* img */}
-              <img className='group-hover:scale-125 transition-all duration-500' src={Img1} alt="" />
+              <img 
+              className="group-hover:scale-125 transition-all duration-500" 
+              src={Img3} 
+              alt="" 
+              />
               {/* pretitle */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-white'>
-                UI/UX Design
-                </span>
-                </div>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
+                <span className="text-white">UI/UX Design</span>
+              </div>
               {/* title */}
-              <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
-                <span className='text-3xl text-white'>Project Title</span>
+              <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
+                <span className="text-3xl text-white">Project Title</span>
               </div>
             </div>
-          </div>
+
+          </motion.div>
         </div>
       </div>
-      </section>
-  )
+    </section>
+  );
 }
