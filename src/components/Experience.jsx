71cbed3bdf71ea2f584853ba1ecac4899f3a04d9
Juhniel <juhn.sweden@gmail.com>
2023-05-15 01:04:35 +0200

    experience text and moved to jsonData

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,33 +4,9 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../variants";
 import TextAnimation from "./TextAnimation";
 import TextAnimation2 from "./TextAnimationFishEye";
+import experienceData from "../data/experienceData.json";
 
-
-const experience = [
-  {
-    name: "Capgemini",
-    title: "Intern - Software Engineer ",
-    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum repellat quidem maiores quaerat ipsum soluta omnis numquam eligendi! Atque, corporis!",
-    date: "Jun. 2023 - Aug. 2023",
-    place: "Malmö, Sweden",
-  },
-  {
-    name: "Grit Academy",
-    title: "Student",
-    description: "Java as programming language with courses within front-end & back-end development.",
-    date: "Aug. 2022 - Jun. 2024",
-    place: "Malmö, Sweden",
-  },
-  {
-    name: "Sweprima AB",
-    title: "Administration & Coordinator",
-    description: "Primary responsibilities involved managing the financial aspects of the company. Bookkeeping and invoices including payroll for the employees.",
-    date: "Aug. 2022 - Jun. 2024",
-    place: "Halmstad, Sweden",
-  },
-];
-
-export default function Experience({scrollToElement, showAllProjects}) {
+export default function Experience({ scrollToElement, showAllProjects }) {
   const experienceTitle = "Experience".split("");
   const download = "Download CV".split("");
   const portfolio = "Portfolio".split("");
@@ -63,33 +39,48 @@ export default function Experience({scrollToElement, showAllProjects}) {
               })}
             </motion.h1>
             <h3 className="text-secondary text-black dark:text-white max-w[455px] mb-12 ml-2">
-             I have been programming since July 2022. 
+              As a highly motivated individual with a passion for learning and
+              self-improvement, I have gained valuable experience in the world
+              of programming since July 2022. My diverse skill set includes
+              languages such as Java & JavaScript aswell as front-end technologies 
+              like React and CSS. Alongside this,I have also developed a strong 
+              understanding of back-end tools, including Spring and SQL databases. 
+              I continuously strive to expand my knowledge and stay up-to-date with the latest
+              industry trends, enabling me to deliver cutting-edge solutions in
+              my projects. 
+              
+              <br/>
+              <br/>
+              
+              - Juhn K
             </h3>
             <button className="btn btn-sm btn-hover text-base">
               <a href={cv} download>
-            {download.map((letter, index) => {
-                return (
-                  <TextAnimation2 key={index}>
-                    {letter === " " ? "\u00A0" : letter}
-                  </TextAnimation2>
-                );
-              })}
+                {download.map((letter, index) => {
+                  return (
+                    <TextAnimation2 key={index}>
+                      {letter === " " ? "\u00A0" : letter}
+                    </TextAnimation2>
+                  );
+                })}
               </a>
             </button>
-            <button 
-            onClick={() => scrollToElement(showAllProjects ? "all-projects" : "latest-projects")}
-            className="btn btn-sm btn-hover text-base ml-3">
-              
-            {portfolio.map((letter, index) => {
+            <button
+              onClick={() =>
+                scrollToElement(
+                  showAllProjects ? "all-projects" : "latest-projects"
+                )
+              }
+              className="btn btn-sm btn-hover text-base ml-3"
+            >
+              {portfolio.map((letter, index) => {
                 return (
                   <TextAnimation2 key={index}>
                     {letter === " " ? "\u00A0" : letter}
                   </TextAnimation2>
                 );
               })}
-        
             </button>
-            
           </motion.div>
           {/* experience */}
           <motion.div
@@ -101,7 +92,7 @@ export default function Experience({scrollToElement, showAllProjects}) {
           >
             {/* experience list */}
             <div className="text mt-8 h-30">
-              {experience.map((experience, index) => {
+              {experienceData.map((experience, index) => {
                 const { name, description, title, date, place } = experience;
                 return (
                   <div
@@ -112,9 +103,7 @@ export default function Experience({scrollToElement, showAllProjects}) {
                       <h4 className="text-[20px] tracking-wider font-primary font-semibold">
                         {name}
                       </h4>
-                      <p className="text font-thin mb-6">
-                        {title}
-                      </p>
+                      <p className="text font-thin mb-6">{title}</p>
                       <p className="font-secondary leading-tight mb-12">
                         {description}
                       </p>
