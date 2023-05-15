3bac3c257dbb1f0c0fe3231e5a9afbae42265b22
Juhniel <juhn.sweden@gmail.com>
2023-05-15 23:28:17 +0200

    test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:  '/Portfolio-reactv2/',
+  // base:  '/Portfolio-reactv2/',
   // build: {
   //   outDir: "docs",
   // },
