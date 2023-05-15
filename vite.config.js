fa837c6252ba778a87eaf59afe9d5c429d514cb0
Juhniel <juhn.sweden@gmail.com>
2023-05-15 22:50:15 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const repoBase = process.env.NODE_ENV === 'production' ? '/Portfolio-reactv2/' : '/';
+// const repoBase = process.env.NODE_ENV === 'production' ? '/Portfolio-reactv2/' : '/';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: repoBase,
-  build: {
-    outDir: "docs",
-  },
+  base:  '/Portfolio-reactv2/',
+  // build: {
+  //   outDir: "docs",
+  // },
 })
